0a74343a3736807af30f38e536778938cb18e7f5
Dan Manning <dmanning23@hotmail.com>
2019-06-08 12:52:24 -0500

    Initial commit

---
+++ MonoGameInsideUIView.iOS/ViewController.cs
@@ -0,0 +1,39 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace MonoGameInsideUIView.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+		public void RunGame()
+		{
+			var game = new Game1(Finish);
+
+			game.Run();
+		}
+
+		public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			RunGame();
+		}
+
+		public void Finish()
+		{
+			this.DismissViewController(false, null);
+		}
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
